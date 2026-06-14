382e1f4ad024d03fefb0eaf35b42ce065401a6a3
Chinchinie <lethidiemtrinh7799@gmail.com>
2026-06-14 22:53:52 +0700

    Implement loyalty customer discount feature

--- test_discount.py
+++ test_discount.py
@@ -5,9 +5,3 @@ def test_vip_customer():
 
 def test_normal_customer():
     assert calculate_discount(30000000) == 0
-
-def test_tc03():
-    # Tổng trước: 49M
-    # Đơn hàng mới: 2M
-    # Tổng sau: 51M => theo yêu cầu nghiệp vụ phải được giảm 10%
-    assert calculate_discount(49000000) == 0.1
